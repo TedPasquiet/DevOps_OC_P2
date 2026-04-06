727ed9b79192e8bf04d0f5d4568a47d823a64033
TedPasquiet <pasquietted@gmail.com>
2026-04-06 19:19:52 +0200

    backend mise en place du JWT

--- Back-end---Testez-et-am-liorez-une-application-existante/src/main/java/com/openclassrooms/etudiant/service/JwtService.java
+++ Back-end---Testez-et-am-liorez-une-application-existante/src/main/java/com/openclassrooms/etudiant/service/JwtService.java
@@ -1,14 +1,31 @@
 package com.openclassrooms.etudiant.service;
 
-
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
 
 @Service
 public class JwtService {
 
-    public String generateToken(UserDetails userDetails) {
-        return null; // TODO
-    }
+    @Value("${jwt.secret}")
+    private String secret;
 
+    @Value("${jwt.expiration}")
+    private long expiration;
+
+    public String generateToken(UserDetails userDetails) {
+        Key key = Keys.hmacShaKeyFor(secret.getBytes());
+        return Jwts.builder()
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
 }
